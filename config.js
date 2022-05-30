6a6697503fcde0e41d1f40a5d6764db2fac36c63
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-05-31 06:06:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -74,7 +74,7 @@ global.eror = 'etto..gomenasai server nya error desu'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
 // tingkat kesulitan, semakin tinggi semakin susah
-global.multiplier = 100 // The higher, The harder levelup
+global.multiplier = 1000 // The higher, The harder levelup
 
 //*****************PEMBATAS*********************
 // JANGAN DI GANTI NTAR KLO GABISA JAN TANYA GW
