c8d7bd8578ffbddb8990109ce846949ca8428774
Arullzzz <106241765+Arullzzz@users.noreply.github.com>
2022-05-31 06:05:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = 'bit.ly/officialgrupkanaobot'
 gc2 = ''
 gc3 = ''
 global.linkGC = ['bit.ly/officialgrupkyurabot'] // ganti jadi group lu
-global.owner = ['6281998903280'] // Put your number here //owner eval
-global.kontak = ['6281998903280'] //Ketika ada yang ngetik #owner
-global.mods = ['6281998903280'] // Want some help?
-global.prems = ['6281998903280'] // Premium user has unlimited limit
+global.owner = ['6285822347348'] // Put your number here //owner eval
+global.kontak = ['6285822347348'] //Ketika ada yang ngetik #owner
+global.mods = ['6285822347348'] // Want some help?
+global.prems = ['6285822347348'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -54,14 +54,14 @@ namakontak2 = 'Ardhi'
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'From' // ganti aja
-global.author = 'KyuraBOT' // ganti aja
+global.packname = 'Arull' // ganti aja
+global.author = 'ArullBotz' // ganti aja
 
 //yyy
-bc = 'KanaoBOT' //RFK Broadcast
-footer = '\n©Kyura'
-namabot = 'KanaoBOT'
-namalu = 'Kyura'
+bc = 'ArullBotz' //Arull Broadcast
+footer = '\n©Arull'
+namabot = 'ArullBotz'
+namalu = 'Arull'
 
 
 // 
